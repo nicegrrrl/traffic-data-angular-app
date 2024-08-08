ca6bfd4e603cedc8227c22c23b9f15037d072aae
Alana Maia <alanaalvesmaia@gmail.com>
2024-08-08 13:35:26 -0300

    multi element custom components and content projection

--- src/app/shared/button/button.component.ts
+++ src/app/shared/button/button.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   // selector: 'app-button', // this renders the tag <app-button> in the DOM
-  selector: 'button[appButton]', // controls buttons that have the attribute appButton; component selector -> attribute selector
+  selector: 'button[appButton, a[appButton]', // controls buttons that have the attribute appButton; component selector -> attribute selector
   standalone: true,
   imports: [],
   templateUrl: './button.component.html',
