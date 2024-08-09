8b705b7b924f97880516c348876d13e833661675
Alana Maia <alanaalvesmaia@gmail.com>
2024-08-09 13:36:08 -0300

    host feature

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,4 +1,13 @@
-import { Component, Input, input, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostBinding,
+  HostListener,
+  inject,
+  Input,
+  input,
+  ViewEncapsulation,
+} from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -7,9 +16,24 @@ import { Component, Input, input, ViewEncapsulation } from '@angular/core';
   templateUrl: './control.component.html',
   styleUrl: './control.component.css',
   encapsulation: ViewEncapsulation.None,
+  host: {
+    class: 'control',
+    '(click)': 'onClick()',
+  },
 })
 export class ControlComponent {
+  // @HostBinding('class') className = 'control';
+  // @HostListener('click') onClick() {
+  //   console.log('Clicked!');
+  // }
+
   label = input.required<string>();
+  private el = inject(ElementRef);
 
   // @Input({ required: true }) label: string;
+
+  onClick() {
+    console.log('Clicked!');
+    console.log(this.el);
+  }
 }
