79039783f9e7cd90605b2ac53421fbe0ab192d09
Alana Maia <alanaalvesmaia@gmail.com>
2024-08-09 14:45:56 -0300

    ngOnInit

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,6 +7,27 @@ import { Component } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css',
 })
-export class ServerStatusComponent {
-  currentStatus = 'online';
+export class ServerStatusComponent implements OnInit {
+  currentStatus: 'offline' | 'online' | 'unknown' = 'online';
+  // setting specific values as types uses a TypeScript feature called
+  // "Literal Types". This idea is to only allow specific (string) values,
+  // instead of all strings.
+
+  constructor() {}
+  // it's considered a good pratice in Angular applications to keep the constructor lean,
+  // and only do basic class initialization in there
+
+  ngOnInit() {
+    setInterval(() => {
+      const rnd = Math.random();
+
+      if (rnd < 0.5) {
+        this.currentStatus = 'online';
+      } else if (rnd < 0.9) {
+        this.currentStatus = 'offline';
+      } else {
+        this.currentStatus = 'unknown';
+      }
+    }, 5000);
+  }
 }
