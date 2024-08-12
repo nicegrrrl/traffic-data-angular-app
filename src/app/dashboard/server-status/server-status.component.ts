f6baf17c345beb0d5269fb16762bca1077364c46
Alana Maia <alanaalvesmaia@gmail.com>
2024-08-12 08:58:58 -0300

    ngOnDestroy, DestroyRef

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  DestroyRef,
+  inject,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,18 +13,24 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css',
 })
+// export class ServerStatusComponent implements OnInit, OnDestroy {
 export class ServerStatusComponent implements OnInit {
   currentStatus: 'offline' | 'online' | 'unknown' = 'online';
   // setting specific values as types uses a TypeScript feature called
   // "Literal Types". This idea is to only allow specific (string) values,
   // instead of all strings.
 
+  // private interval?: ReturnType<typeof setInterval>;
+
+  private destroyRef = inject(DestroyRef);
+
   constructor() {}
   // it's considered a good pratice in Angular applications to keep the constructor lean,
   // and only do basic class initialization in there
 
   ngOnInit() {
-    setInterval(() => {
+    // this.interval = setInterval(() => {
+    const interval = setInterval(() => {
       const rnd = Math.random();
 
       if (rnd < 0.5) {
@@ -29,5 +41,13 @@ export class ServerStatusComponent implements OnInit {
         this.currentStatus = 'unknown';
       }
     }, 5000);
+
+    this.destroyRef.onDestroy(() => {
+      clearInterval(interval);
+    });
   }
+
+  // ngOnDestroy() {
+  //   clearTimeout(this.interval);
+  // }
 }
